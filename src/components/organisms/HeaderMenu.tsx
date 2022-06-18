3d20307111cd94e08d7fadd8da3b2ab7d4e71afb
Sayaka Koyama <61900280+wannyanland@users.noreply.github.com>
2022-06-18 22:16:54 +0900

    Nextjs ディレクトリ構成修正

---
+++ src/components/organisms/HeaderMenu.tsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+export default function HeaderMenu() {
+  return (
+    <div>
+      <ul>
+        <li>プロフィール</li>
+        <li>日記</li>
+        <li>ギャラリ〜</li>
+      </ul>
+    </div>
+  )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import HeaderMenu from '../components/organisms/HeaderMenu'
 
 export default function Home() {
   return (
@@ -12,6 +13,7 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
+        <HeaderMenu></HeaderMenu>
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
