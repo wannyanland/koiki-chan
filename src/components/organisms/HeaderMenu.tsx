572917d439d20d667060aa8be8cfc2e6a7451e65
Sayaka Koyama <61900280+wannyanland@users.noreply.github.com>
2022-06-20 21:55:46 +0900

    ヘッダーメニュー作成

--- src/components/organisms/HeaderMenu.tsx
+++ src/components/organisms/HeaderMenu.tsx
@@ -1,27 +1,38 @@
 import React from "react"
 import styled from "styled-components"
 
-const Header = styled.div`
-display: block;
-`;
-
 const List = styled.ul`
+padding: 0;
 display: flex;
 `;
 
 const Item = styled.li`
 list-style-type: none;
+min-width: 200px;
+padding: 10px;
+text-align: center;
+border: 5px solid #808080;
+border-radius: 5px 5px 0 0;
+:nth-child(2n) {
+  margin: 0 2px 0 2px;
+}
+:hover {
+  border-color: transparent;
+  background-color: #43919B;
+  color: white;
+  font-weight: bold;
+}
 `;
 
 export default function HeaderMenu() {
 
   return (
-    <Header>
+    <div>
       <List>
         <Item>プロフィール</Item>
-        <Item>日記</Item>
-        <Item>ギャラリ〜</Item>
+        <Item>にっき</Item>
+        <Item>ギャラﾘ</Item>
       </List>
-    </Header>
+    </div>
   )
 }
